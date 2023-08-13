7ea8228333550429bd5a9dadfa18230bb32d203a
RaHoni <honisuess@gmail.com>
2023-08-13 12:59:47 +0200

    Enable Firewall

--- configuration.nix
+++ configuration.nix
@@ -174,7 +174,7 @@ sansSerif = ["MesloLGS NF Regular"];
   networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
