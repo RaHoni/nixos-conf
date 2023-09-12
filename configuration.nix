00e7590f6ea450dc2e2c97085a9f60d186039035
RaHoni <honisuess@gmail.com>
2023-09-12 15:31:36 +0200

    Added a basic Nextcloud config
    This Nextcloud was used to sync Podcasts via gpoddersync plugin

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@ in
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./bacula.nix
+      #./nextcloud.nix
       #./copy_usb.nix
       ./users.nix
 #       <home-manager/nixos>
