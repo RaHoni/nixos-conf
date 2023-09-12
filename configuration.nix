8861437133fc249863d8d9c8188245f98fd5b33b
RaHoni <honisuess@gmail.com>
2023-09-12 15:27:27 +0200

    Disable Firewall

--- configuration.nix
+++ configuration.nix
@@ -165,10 +165,10 @@ in
 
   programs.kdeconnect.enable = true;
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = true;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
