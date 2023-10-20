88060fbcd6e74e0cb475b645874cc830675e5683
RaHoni <honisuess@gmail.com>
2023-10-20 11:34:22 +0200

    Don't import ssh_hostkeys in sops
    This fixes an issue where we would try to import the ssh-hostkey while
    we didn't have any since they are in sops. This gets fixed by clearing
    the age.sshKeyPaths option.

--- generic/sops.nix
+++ generic/sops.nix
@@ -1,9 +1,15 @@
 { config, ... }:
 {
-sops = {
+  sops = {
     defaultSopsFile = ../secrets/general.yaml;
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    age.generateKey = true; #generate key above if it does not exist yet (has to be added manually to .sops.yaml)
-    secrets.example-key = {};
+    age = {
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true; #generate key above if it does not exist yet (has to be added manually to .sops.yaml)
+      sshKeyPaths = [ ];
+    };
+    secrets.example-key = { };
+    gnupg.sshKeyPaths = [ ];
+
   };
 }
+
