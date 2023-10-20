d0936732df99c1767dcd2b432a9b1a7ed39cfc60
RaHoni <honisuess@gmail.com>
2023-10-20 14:22:09 +0200

    Removing unneeded overlay definition

--- surface-raoul-nixos/configuration.nix
+++ surface-raoul-nixos/configuration.nix
@@ -114,28 +114,6 @@ in
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      zsh-powerlevel10k = prev.zsh-powerlevel10k.overrideAttrs {
-        #    pname = "powerlevel10k-raoul";
-        installPhase = ''
-          install -D powerlevel10k.zsh-theme --target-directory=$out/share/zsh/themes/powerlevel10k
-          install -D powerlevel9k.zsh-theme --target-directory=$out/share/zsh/themes/powerlevel10k
-          install -D config/* --target-directory=$out/share/zsh/themes/powerlevel10k/config
-          install -D internal/* --target-directory=$out/share/zsh/themes/powerlevel10k/internal
-          cp -R gitstatus $out/share/zsh/themes/powerlevel10k/gitstatus
-        '';
-      };
-      zsh-nix-shell = prev.zsh-nix-shell.overrideAttrs {
-        installPhase = ''
-          install -D nix-shell.plugin.zsh --target-directory=$out/share/zsh/plugins/nix-shell
-          install -D scripts/* --target-directory=$out/share/zsh/plugins/nix-shell/scripts
-        '';
-      };
-    }
-    )
-  ];
-
   programs.zsh = {
     enable = true;
 
