74b8507542093990178f2715841f2b5614f444b1
RaHoni <honisuess@gmail.com>
2023-10-20 13:05:23 +0200

    Move Overlays to own file for sharing between hosts

--- flake.nix
+++ flake.nix
@@ -28,25 +28,7 @@
         system = "x86_64-linux";
         pkgs = import nixpkgs-stable {
           overlays = [
-            (final: prev: {
-              zsh-powerlevel10k = prev.zsh-powerlevel10k.overrideAttrs {
-                #    pname = "powerlevel10k-raoul";
-                installPhase = ''
-                  install -D powerlevel10k.zsh-theme --target-directory=$out/share/zsh/themes/powerlevel10k
-                  install -D powerlevel9k.zsh-theme --target-directory=$out/share/zsh/themes/powerlevel10k
-                  install -D config/* --target-directory=$out/share/zsh/themes/powerlevel10k/config
-                  install -D internal/* --target-directory=$out/share/zsh/themes/powerlevel10k/internal
-                  cp -R gitstatus $out/share/zsh/themes/powerlevel10k/gitstatus
-                '';
-              };
-              zsh-nix-shell = prev.zsh-nix-shell.overrideAttrs {
-                installPhase = ''
-                  install -D nix-shell.plugin.zsh --target-directory=$out/share/zsh/plugins/nix-shell
-                  install -D scripts/* --target-directory=$out/share/zsh/plugins/nix-shell/scripts
-                '';
-              };
-            }
-            )
+            (import ./generic/overlays.nix)
           ];
           inherit system;
           config = {
