4e3685ae3b44004fcf4c91c4844d8b6b0c47bcf0
RaHoni <honisuess@gmail.com>
2023-10-25 21:46:22 +0200

    Change authorized ssh-key of raspberry

--- raspberry/configuration.nix
+++ raspberry/configuration.nix
@@ -13,7 +13,7 @@
 
   users.users.root = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/3EQ9XhwTdsWUSmpBfjqKxPFfeFg/RArJ1uZSZf3fm Surface"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL2OK6+2Gk7zJQH0dPpyl4wNdgzLI9U6VNXZQjFyVfR+ SSH for Raspberry-Pi-Raoul"
     ];
   };
 
