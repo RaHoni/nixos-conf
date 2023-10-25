445587855939dd6a13ed0de86a27adde76ccbd5a
RaHoni <honisuess@gmail.com>
2023-10-25 19:41:53 +0200

    Enable ss

--- raspberry/configuration.nix
+++ raspberry/configuration.nix
@@ -29,6 +29,8 @@
     git
   ];
 
+  services.openssh.enable = true
+
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
