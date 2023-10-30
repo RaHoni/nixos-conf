fa2100a2f11039c2c82b18103e1ba7b891d4ccd4
RaHoni <honisuess@gmail.com>
2023-10-30 19:15:57 +0100

    Add port 53 udp to the firewall of raspberry

--- raspberry/configuration.nix
+++ raspberry/configuration.nix
@@ -35,8 +35,8 @@
 
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ... ];
+  networking.firewall.allowedUDPPorts = [ 53 ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
