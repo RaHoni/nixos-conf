db291986f6be05b54c0a03506d10e9e595b283b9
RaHoni <honisuess@gmail.com>
2023-11-10 13:13:49 +0100

    Added basic first config for r-dektop

---
+++ r-desktop/users.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  users.users.raoul = {
+    isNormalUser = true;
+    description = "Raoul Honermann";
+    hashedPassword = "$y$j9T$2qmWuo6/DJXoG.45LLjDX/$Y/NnNHfsQXULwubyI1lPavjfe3fYv/KTWMR4aPLhsSB";
+    extraGroups = [ "networkmanager" "wheel" "i2c" ];
+    packages = with pkgs; [
+      keepassxc
+      jetbrains.webstorm
+      jetbrains.intelij
+      jetbrains.clion
+      kmail
+      nixos-generators
+      kleopatra
+      whatsapp-for-linux
+      rnix-lsp
+      gh
+      tetex
+      nixpkgs-fmt
+      pre-commit
+      maliit-keyboard
+      jq
+      kdiff3
+      sops
+      kaddressbook
+      yubioath-flutter
+      nebula
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/3EQ9XhwTdsWUSmpBfjqKxPFfeFg/RArJ1uZSZf3fm Surface"
+    ];
+  };
+}
