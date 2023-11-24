73c4202d72186b43176091f82a7967910cb976ae
RaHoni <honisuess@gmail.com>
2023-11-24 09:13:37 +0100

    Add extra caching server

--- flake.nix
+++ flake.nix
@@ -22,6 +22,17 @@
 
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
   outputs = { self, ... }@inputs:
     with inputs;
     let
@@ -143,6 +154,7 @@
 
     };
 }
+
 
 
 
