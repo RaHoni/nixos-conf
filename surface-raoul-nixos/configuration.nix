c25ff8ba7b724ce21992881fca5d3861c148b620
RaHoni <honisuess@gmail.com>
2023-12-01 10:46:28 +0100

    Update pam to use the yubikey also in polkit

--- surface-raoul-nixos/configuration.nix
+++ surface-raoul-nixos/configuration.nix
@@ -139,6 +139,7 @@ in
   security.pam.services = {
     login.u2fAuth = true;
     sudo.u2fAuth = true;
+    polkit-1.u2fAuth = true;
   };
 
   # This value determines the NixOS release from which the default
