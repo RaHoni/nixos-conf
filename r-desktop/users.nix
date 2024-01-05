45da2922063727f2752bca0f93191609ef59f7ef
RaHoni <honisuess@gmail.com>
2024-01-05 22:49:24 +0100

    r-desktop raoul: add prismlauncher
    Prismlauncher is a custom minecraft Launcher

--- r-desktop/users.nix
+++ r-desktop/users.nix
@@ -34,6 +34,7 @@
       nebula
       platformio-core
       android-studio
+      prismlauncher
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/3EQ9XhwTdsWUSmpBfjqKxPFfeFg/RArJ1uZSZf3fm Surface"
