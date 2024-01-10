49a584ee6f8ea87f76574adcc542cedb65188989
RaHoni <honisuess@gmail.com>
2024-01-10 20:26:59 +0100

    sops: Use option to set openssh hostkeys not paths

--- generic/sops.nix
+++ generic/sops.nix
@@ -16,13 +16,21 @@
       yubikey-auths = { };
       # Setup ssh-Hostkeys from sops
       "ssh_host_ed25519_key" = {
-        path = "/etc/ssh/ssh_host_ed25519_key";
         sopsFile = ../secrets/${config.networking.hostName}/sshd.yaml;
       };
       "ssh_host_rsa_key" = {
-        path = "/etc/ssh/ssh_host_rsa_key";
         sopsFile = ../secrets/${config.networking.hostName}/sshd.yaml;
       };
+      services.openssh.hostKeys = [
+        {
+          path = config.sops.secrets."ssh_host_ed25519_key".path;
+          type = "ed25519";
+        }
+        {
+          path = config.sops.secrets."ssh_host_rsa_key".path;
+          type = "rsa";
+        }
+      ];
     };
   };
 }
