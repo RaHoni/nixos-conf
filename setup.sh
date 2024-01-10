9be53a30a3b146dd44cac013765232e7cf1f0606
RaHoni <honisuess@gmail.com>
2024-01-10 17:00:51 +0100

    Setup: Add a first setup script for adding SSH Keys
    This file should automate a lot of the tasks when adding a new host or
    deploying a config to a new host.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash --packages git sops yq-go
+
+hostname=""
+
+sshKeys() {
+    mkdir -p secrets/$hostname
+    ssh-keygen -t ed25519 -N "" -f /tmp/ed25519
+    ssh-keygen -t rsa -N "" -f /tmp/rsa
+    ed25519key=$(</tmp/ed25519)
+    rsakey=$(</tmp/rsa)
+    echo \{\"ssh_host_ed25519_key\": \"$ed25519key\",\"ssh_host_rsa_key\": \"$rsakey\"\} | yq -p json > secrets/$hostname/sshd.yaml
+    sops -i -e secrets/$hostname/sshd.yaml
+    git add secrets/ssl-proxy/sshd.yaml
+    git commit -o secrets/ssl-proxy/sshd.yaml -m "$hostname: Added ssh hostkeys for $hostname"
+    }
+    
+
+cd $(dirname "$0")
+pwd
+case $1 in
+    sshKeys)
+    hostname=$2
+    sshKeys
+    exit 0
+    ;;
+    *)
+    exit 1
+    ;;
+esac
