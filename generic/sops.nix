460c16931c018982aea16e14eb88f6edc852702a
RaHoni <honisuess@gmail.com>
2024-01-12 11:34:26 +0100

    Fix: SSH Hostkey

--- generic/sops.nix
+++ generic/sops.nix
@@ -21,16 +21,6 @@
       "ssh_host_rsa_key" = {
         sopsFile = ../secrets/${config.networking.hostName}/sshd.yaml;
       };
-      services.openssh.hostKeys = [
-        {
-          path = config.sops.secrets."ssh_host_ed25519_key".path;
-          type = "ed25519";
-        }
-        {
-          path = config.sops.secrets."ssh_host_rsa_key".path;
-          type = "rsa";
-        }
-      ];
     };
   };
   services.openssh.hostKeys = [
