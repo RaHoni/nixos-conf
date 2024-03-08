6aba42161b29af0fb589c76dd39c48843c933425
RaHoni <honisuess@gmail.com>
2024-03-08 09:57:02 +0100

    flake: Add cachix rahoni

--- flake.nix
+++ flake.nix
@@ -39,14 +39,17 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
+      "https://rahoni.cachix.org"
     ];
     extra-trusted-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
+      "https://rahoni.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "rahoni.cachix.org-1:iCKI8r6HT5rToodXfecglGJnPTaOaGzNeAS5wawMuMM="
     ];
   };
 
