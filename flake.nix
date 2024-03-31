fff15d92c933ad0f869ca3c2c9e8375f8338b295
RaHoni <honisuess@gmail.com>
2024-03-31 20:54:45 +0200

    Revert "flake.nix: Remove nixpkgs-ffmpeg"
    This reverts commit 1fb36dc137d1a479b5ec44f4841a358c6c65bd78.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-ffmpeg.url = "github:evanrichter/nixpkgs/libvpl-for-intel-gpu-cp";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -69,7 +70,7 @@
       overlays = system: final: prev: {
         stable = pkgsConfig nixpkgs-stable system;
         unstable = pkgsConfig nixpkgs system;
-        ffmpeg-vpl = import nixpkgs {
+        ffmpeg-vpl = import nixpkgs-ffmpeg {
           overlays = [ (import ./generic/overlays/ffmpeg.nix) ];
           inherit system;
           config.allowUnfree = true;
