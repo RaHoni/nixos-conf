01a5dc64cdcb68958d45f51b2ed03165ed0eb97a
RaHoni <honisuess@gmail.com>
2024-04-28 11:27:45 +0200

    PetronillaStreaming add driver for wifistick

--- petronillaStreaming/hardware-configuration.nix
+++ petronillaStreaming/hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl88x2bu ];
 
   fileSystems."/" =
     {
