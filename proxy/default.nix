5039cad0d599d6e9da14caf229aeaee5899764ab
RaHoni <honisuess@gmail.com>
2024-05-03 10:33:29 +0200

    ssl-proxy: explicit enabling of sshd not needed

--- proxy/default.nix
+++ proxy/default.nix
@@ -3,9 +3,6 @@
   networking.hostName = "ssl-proxy";
   networking.firewall.allowPing = true;
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-  services.sshd.enable = true;
-  services.openssh.enable = true;
-  services.openssh.openFirewall = true;
   #networking.firewall.enable = lib.mkForce false;
   system.stateVersion = "23.11";
 }
