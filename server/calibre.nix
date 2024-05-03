6d0304db745249a4d76bf24122e5469495e6c394
RaHoni <honisuess@gmail.com>
2024-05-03 12:41:34 +0200

    server: Start unifing servers

---
+++ server/calibre.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  services.calibre-server = {
+    enable = true;
+    auth = {
+      enable = true;
+      userDb = /statefull/calibre/users.sqlite;
+    };
+    libraries = /srv/ebooks;
+  };
+}
