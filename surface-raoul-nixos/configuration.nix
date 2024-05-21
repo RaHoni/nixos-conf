2ce26a21a58b8542965efa8a0ba9ca4a2e9b74af
RaHoni <honisuess@gmail.com>
2024-05-21 17:10:18 +0200

    surface: Add kdenlive

--- surface-raoul-nixos/configuration.nix
+++ surface-raoul-nixos/configuration.nix
@@ -101,6 +101,7 @@
     ffmpeg
     nodePackages.bash-language-server
     
+    libsForQt5.kdenlive
     neovim
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
