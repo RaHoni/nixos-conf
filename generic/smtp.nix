9231bb5ef11647909e789c812c714ea7dd6564c3
RaHoni <honisuess@gmail.com>
2024-06-27 20:28:04 +0200

    server: Add postfix as general smtp relay

---
+++ generic/smtp.nix
@@ -0,0 +1,31 @@
+{ config, ... }:
+{
+  sops.secrets.smtp-password.sopsFile = ../secrets/smtp.yaml;
+  sops.templates.postfix-sasl_passwd.content = "[smtp.web.de]:587 honermann.info@web.de:${config.sops.placeholder.smtp-password}";
+  services.postfix = {
+    enable = true;
+    relayHost = "smtp.web.de";
+    relayPort = 587;
+    rootAlias = "raoul.honermann@web.de";
+    enableHeaderChecks = false;
+    canonical = ''
+      /^(.*)<.*>$/     $1 <honermann.info@web.de>
+      /^([^<>]+)$/     $1 <honermann.info@web.de>
+    '';
+    headerChecks = [
+      {
+        pattern = "/^From:([\w\s]*)<?(\w*)@(\w)*/";
+        action = "REPLACE From: $1 $2 $3 <honermann.info@web.de>";
+      }
+    ];
+    config = {
+      smtp_use_tls = "yes";
+      smtp_sasl_auth_enable = "yes";
+      smtp_sasl_security_options = "";
+      smtp_sasl_password_maps = "texthash:${config.sops.templates."postfix-sasl_passwd".path}";
+      sender_canonical_maps = "regexp:/var/lib/postfix/conf/canonical";
+      # optional: Forward mails to root (e.g. from cron jobs, smartd)
+      # to me privately and to my work email:
+    };
+  };
+}
