27f44e611a53ebd813a7e2030e03b21c695b1196
RaHoni <honisuess@gmail.com>
2024-06-30 20:54:28 +0200

    pim: trying to fix pim in Plasma6

--- generic/pim.nix
+++ generic/pim.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs.kdePackages; [
-    kdepim-addons
-    kdepim-runtime
-    kdeplasma-addons
     akonadi
     akonadi-calendar
     akonadi-calendar-tools
     akonadi-contacts
     akonadi-search
-    korganizer
+    calendarsupport
     kaddressbook
+    kcalendarcore
+    kdepim-addons
+    kdepim-runtime
+    kdeplasma-addons
+    korganizer
+    libkdepim
+    pimcommon
   ];
 }
