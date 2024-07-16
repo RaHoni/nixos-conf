a077a77ff7a483f66c3fbba2a8479fa8c28838c3
RaHoni <honisuess@gmail.com>
2024-07-16 10:54:25 +0200

    flake: Reintroduse bacula private rep

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-bacula.url = "github:RaHoni/nixpkgs/bacula";
 
     disko = {
       url = "github:nix-community/disko";
