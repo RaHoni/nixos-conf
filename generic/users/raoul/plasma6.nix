df5c3f945941df661bbb3bbe5097d1ff73ca7fa6
RaHoni <honisuess@gmail.com>
2024-07-16 10:50:33 +0200

    raoul/plasma6: Fix shortcut definition

--- generic/users/raoul/plasma6.nix
+++ generic/users/raoul/plasma6.nix
@@ -8,10 +8,10 @@
 
     shortcuts = {
       kwin = {
-        "Switch to Next Desktop" = "Meta+Tab,,Switch to Next Desktop";
-        "Switch to Previous Desktop" = "Meta+Shift+Tab,,Switch to Previous Desktop";
-        "Window to Next Desktop" = "Meta+Ctrl+Tab,,Window to Next Desktop";
-        "Window to Previous Desktop" = "Meta+Ctrl+Shift+Tab,,Window to Previous Desktop";
+        "Switch to Next Desktop" = "Meta+Tab";
+        "Switch to Previous Desktop" = "Meta+Shift+Tab";
+        "Window to Next Desktop" = "Meta+Ctrl+Tab";
+        "Window to Previous Desktop" = "Meta+Ctrl+Shift+Tab";
       };
     };
 
