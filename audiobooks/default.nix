4bb4a6c2548b78582a7d69e58b110db92d427d8a
RaHoni <honisuess@gmail.com>
2024-08-30 08:41:49 +0200

    audiobooks: Use unstable for audiobookshelf

--- audiobooks/default.nix
+++ audiobooks/default.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   networking.hostName = "audiobooks";
-  services.audiobookshelf.enable = true;
-  services.audiobookshelf.openFirewall = true;
-  services.audiobookshelf.host = "0.0.0.0";
+  services.audiobookshelf = {
+    enable = true;
+    openFirewall = true;
+    host = "0.0.0.0";
+    package = pkgs.unstable.audiobookshelf;
+  };
 
   system.stateVersion = "23.11";
 }
