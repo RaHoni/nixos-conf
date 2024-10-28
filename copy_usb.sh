8e02bbcc16f4ffa19c57d3e09c499c9086a6a60f
RaHoni <honisuess@gmail.com>
2024-10-28 09:11:34 +0100

    copy_usb: Update year for Label

--- copy_usb.sh
+++ copy_usb.sh
@@ -1,11 +1,12 @@
 devicepath=$(systemd-escape -up -- $1)
 echo $devicepath
 echo $PATH
+whoami
 [[ $devicepath =~ ^/dev/sd[a-z]1$ ]] || exit 0
     #Close standard output file descriptor
 #    echo "This line will appear in $LOG_FILE, not 'on screen'"
 echo Starting with $devicepath
-fatlabel $devicepath "Freizeit 23"
+fatlabel $devicepath "Freizeit 24"
 mkdir -p /mnt/$devicepath
 mount $devicepath /mnt/$devicepath
 rm -r /mnt/$devicepath/* || true
