b95e74d31066f3862bb2441d40f23d372d2ccdb3
RaHoni <honisuess@gmail.com>
2024-11-07 22:45:58 +0100

    copy_usb: Improve the script for better usablity

--- copy_usb.sh
+++ copy_usb.sh
@@ -1,15 +1,18 @@
-devicepath=$(systemd-escape -up -- $1)
-echo $devicepath
-echo $PATH
+devicepath=$(systemd-escape -up -- "$1")
+start=$(date +%s)
 whoami
-[[ $devicepath =~ ^/dev/sd[a-z]1$ ]] || exit 0
+[[ "$devicepath" =~ ^/dev/sd[a-z]1$ ]] || exit 0
     #Close standard output file descriptor
 #    echo "This line will appear in $LOG_FILE, not 'on screen'"
-echo Starting with $devicepath
-fatlabel $devicepath "Freizeit 24"
-mkdir -p /mnt/$devicepath
-mount $devicepath /mnt/$devicepath
-rm -r /mnt/$devicepath/* || true
-cp -r /usbcopy/* /mnt/$devicepath
-umount $devicepath
-#eject $devicepath
+echo "Starting with $devicepath"
+fatlabel "$devicepath" "Freizeit 24"
+mkdir -p "/mnt/$devicepath"
+mount "$devicepath" "/mnt/$devicepath"
+rm -r "/mnt/$devicepath/*" || true
+cp -rL /usbcopy/* "/mnt/$devicepath"
+umount "$devicepath"
+end=$(date +%s)
+
+runtime=$((end-start))
+echo Runtime $runtime
+#eject "$devicepath"
