62915807fc25071fbc63bec968dcdf94451cc48a
RaHoni <honisuess@gmail.com>
2024-11-11 12:42:29 +0100

    r-desktop: Don't shutdown after backup

--- r-desktop/bacula.nix
+++ r-desktop/bacula.nix
@@ -2,7 +2,7 @@
 {
   imports = [ ../generic/bacula.nix ];
   services.bacula-fd = {
-    shutdownOnFinish = true;
+    shutdownOnFinish = false;
     director."dir.bacula" = {
       password = "l8HJEqyudH9Fy4JxrXs6JoD9lXgwN+vwJvkF8NJP";
     };
