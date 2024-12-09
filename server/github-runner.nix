2358a31be251004d63cb5ba2cc88f1ab8195a51f
RaHoni <honisuess@gmail.com>
2024-12-09 11:42:16 +0100

    github-runner: Fix labels

--- server/github-runner.nix
+++ server/github-runner.nix
@@ -1,8 +1,9 @@
 { config, ... }:
 {
   sops.secrets.github-runner-key.sopsFile = ../secrets/r-desktop/github.yaml;
-  services.github-runners.runner1 = {
+  services.github-runners.nixos-runner = {
     enable = true;
+    extraLabels = [ "x86_64-linux" ];
     tokenFile = config.sops.secrets.github-runner-key.path;
     url = "https://github.com/RaHoni/nixos-conf";
   };
