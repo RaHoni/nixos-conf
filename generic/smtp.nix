3368759b7d874fc4ad429afeff9e9d1456a2b6c1
RaHoni <honisuess@gmail.com>
2025-01-08 22:10:04 +0100

    mailserver: Add more users
    - christoph
    - sylvia
    - server: This is also setup in smtp

--- generic/smtp.nix
+++ generic/smtp.nix
@@ -1,33 +1,17 @@
 { config, ... }:
 {
   sops.secrets.smtp-password.sopsFile = ../secrets/smtp.yaml;
-  sops.templates.postfix-sasl_passwd.content = "[smtp.web.de]:587 honermann.info@web.de:${config.sops.placeholder.smtp-password}";
+  sops.templates.postfix-sasl_passwd.content = "[mail.honermann.info]:587 server@honermann.info:${config.sops.placeholder.smtp-password}";
   services.postfix = {
     enable = true;
-    relayHost = "smtp.web.de";
+    relayHost = "mail.honermann.info";
     relayPort = 587;
-    rootAlias = "raoul.honermann@web.de";
-    enableHeaderChecks = true;
-    canonical = ''
-      /^(.*)<.*>$/     $1 <honermann.info@web.de>
-      /^([^<>]+)$/     $1 <honermann.info@web.de>
-    '';
-    headerChecks = [
-      {
-        pattern = "/^From:(.*)<.*>/";
-        action = "REPLACE From: $1 <honermann.info@web.de>";
-      }
-      {
-        pattern = "/^From:([^<>@]+)/";
-        action = "REPLACE From: $1 <honermann.info@web.de>";
-      }
-    ];
+    rootAlias = "raoul@honermann.info";
     config = {
       smtp_use_tls = "yes";
       smtp_sasl_auth_enable = "yes";
       smtp_sasl_security_options = "";
       smtp_sasl_password_maps = "texthash:${config.sops.templates."postfix-sasl_passwd".path}";
-      sender_canonical_maps = "regexp:/var/lib/postfix/conf/canonical";
       # optional: Forward mails to root (e.g. from cron jobs, smartd)
       # to me privately and to my work email:
     };
