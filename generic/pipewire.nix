e7253d42e7c075814cf488d40e0c475f3416a614
RaHoni <honisuess@gmail.com>
2025-01-17 21:12:56 +0100

    pipewire: Add echo-cancel

--- generic/pipewire.nix
+++ generic/pipewire.nix
@@ -10,5 +10,20 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+    extraConfig.pipewire."10-echo-cancel"."context.modules" = [
+      {
+        name = "libpipewire-module-echo-cancel";
+        args = {
+          # library.name  = aec/libspa-aec-webrtc
+          # node.latency = 1024/48000
+          source.props = {
+            node.name = "Echo Cancellation Source";
+          };
+          sink.props = {
+            node.name = "Echo Cancellation Sink";
+          };
+        };
+      }
+    ];
   };
 }
