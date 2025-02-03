b81589528b73c5f1187a915f5ed9996b3a51393c
RaHoni <honisuess@gmail.com>
2025-02-03 17:18:49 +0100

    server: Keep unifi data

--- server/unifi.nix
+++ server/unifi.nix
@@ -6,6 +6,9 @@
     unifiPackage = pkgs.unifi;
     mongodbPackage = pkgs.mongodb-7_0;
   };
+  environment.persistence."/permament".directories = [
+    "/var/lib/unifi/"
+  ];
 
   networking.firewall.allowedTCPPorts = [ 8443 ];
 }
