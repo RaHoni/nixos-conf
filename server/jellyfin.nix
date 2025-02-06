03523be2ae544c93eb6b30f0f1f53def5906b5e0
RaHoni <honisuess@gmail.com>
2025-02-06 16:30:15 +0100

    jellyfin: Move /var/lib/jellyfin to permament
    Since Jellyfin does not store the media there this should be stored on
    the ssd

--- server/jellyfin.nix
+++ server/jellyfin.nix
@@ -1,6 +1,15 @@
-{ ... }:
+{ config, ... }:
+let
+  jellyfin = config.services.jellyfin;
+in
 {
   services.jellyfin = {
     enable = true;
   };
+
+  environment.persistence."/permament".directories = with jellyfin; [
+    dataDir
+    configDir
+    logDir
+  ];
 }
