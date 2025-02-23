22c89ccc7680ab7700177e3f2ff9c979c88a7c9f
RaHoni <honisuess@gmail.com>
2025-02-23 17:04:35 +0100

    jasmine: Add nmap

--- jasmine/configuration.nix
+++ jasmine/configuration.nix
@@ -56,6 +56,7 @@
     #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     libreoffice
     wget
+    nmap
     git
     firefox
     gimp
