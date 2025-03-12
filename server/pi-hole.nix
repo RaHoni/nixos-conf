2145cdf89888c0f393fe3322236bdcdf7322a228
RaHoni <honisuess@gmail.com>
2025-03-12 16:05:01 +0100

    pi-hole: Update to v6 and make the setup of adlists easier

--- server/pi-hole.nix
+++ server/pi-hole.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   ips = config.local.ips;
   ipv4 = ips."pi.hole".ipv4;
@@ -6,30 +6,23 @@ let
 in
 {
   imports = [ ../generic/ips.nix ];
-  environment.etc = {
-    "pihole-adlists".source = ./pihole/adlists.list;
-    "pihole-custom".source = ./pihole/custom.list;
-    "dnsmasq-cnames".source = ./pihole/cnames.conf;
-  };
-  systemd.tmpfiles.rules = [
-    # Ensure the /var/pihole/etc-pihole directory exists
-    "d /var/pihole/etc-pihole 0755 root root"
-    "d /var/pihole/etc-dnsmasq.d 0755 root root"
-  ];
-
-  systemd.services."prepare-pihole-dir" = {
-    script = ''
-      cp /etc/pihole-adlists /var/pihole/etc-pihole/adlists.list
-      cp /etc/pihole-custom /var/pihole/etc-pihole/custom.list
-      cp /etc/dnsmasq-cnames /var/pihole/etc-dnsmasq.d/05-pihole-custom-cname.conf
-    '';
-    wantedBy = [ "podman-pi-hole.service" ];
-  };
 
   services.resolved.enable = false;
 
   networking.firewall.allowedTCPPorts = [ 53 ];
   networking.firewall.allowedUDPPorts = [ 53 ];
+
+  systemd.services.pi-hole-adlists = {
+    wants = [ "podman-pi-hole.service" ];
+    script = ''
+      curl -X POST "http://${ipv4}:80/api/lists" -H 'accept: application/json' -H 'content-type: application/json' -d '{"address":["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts","https://raw.githubusercontent.com/0Zinc/easylists-for-pihole/master/easyprivacy.txt","https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt","https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts","https://v.firebog.net/hosts/static/w3kbl.txt","https://adaway.org/hosts.txt","https://v.firebog.net/hosts/AdguardDNS.txt","https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt","https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt","https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&amp;showintro=0&amp;mimetype=plaintext","https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts","https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts","https://v.firebog.net/hosts/Prigent-Ads.txt","https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts","https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt","https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt","https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt","https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt","https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt","https://v.firebog.net/hosts/Prigent-Crypto.txt","https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt","https://phishing.army/download/phishing_army_blocklist_extended.txt","https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt","https://v.firebog.net/hosts/Shalla-mal.txt","https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt","https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts","https://urlhaus.abuse.ch/downloads/hostfile/","https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser","https://raw.githubusercontent.com/ZingyAwesome/easylists-for-pihole/master/language/german.txt"],"type":"block","comment":"Added at setup","groups":[0],"enabled":true}' 
+      podman exec -it pi-hole /usr/local/bin/pihole -g
+    '';
+    path = with pkgs; [
+      curl
+      podman
+    ];
+  };
 
   virtualisation = {
     podman = {
@@ -55,16 +48,25 @@ in
       environment = {
         TZ = "Europe/Berlin";
         FTLCONF_webserver_api_password = "";
+        FTLCONF_dns_upstreams = "208.67.222.222;2620:119:35::35;1.0.0.1;2606:4700:4700::1001";
+        FTLCONF_ = "";
+        #FTLCONF_dns_hosts = ''["192.168.1.207 honermann.info"]''; # simple record
+        FTLCONF_dns_hostRecord = ''["honermann.info,192.168.1.207,"]''; # add ipv4, ipv6 and ptr record
+        FTLCONF_dns_listeningMode = "ALL";
+        FTLCONF_dns_revServers = "true,192.168.0.0/16,192.168.1.1,localdomain";
+        FTLCONF_dns_reply_host_force4 = "true"; # Use specified IP
+        FTLCONF_dns_reply_host_force6 = "true";
+        FTLCONF_dns_reply_host_IPv4 = ipv4;
+        FTLCONF_dns_reply_host_IPv6 = ipv6;
+        FTLCONF_dns_dnssec = "true";
         DNSSEC = "true";
         REV_SERVER = "true";
         REV_SERVER_CIDR = "192.168.1.0/23";
         REV_SERVER_TARGET = "192.168.1.1";
-        PIHOLE_DNS_ = "208.67.222.222;2620:119:35::35;1.0.0.1;2606:4700:4700::1001";
         FTLCONF_LOCAL_IPV4 = ipv4;
       };
       volumes = [
-        "/var/pihole/etc-pihole:/etc/pihole:copy"
-        "/var/pihole/etc-dnsmasq.d:/etc/dnsmasq.d:copy"
+        "etc-pihole:/etc/pihole"
       ];
     };
   };
