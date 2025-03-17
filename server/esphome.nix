e3fcf41a1df1bd352304d55e709c6ae61cf35c8a
RaHoni <honisuess@gmail.com>
2025-03-17 22:45:14 +0100

    server: Open esphome Firewall

--- server/esphome.nix
+++ server/esphome.nix
@@ -3,7 +3,7 @@
   services.esphome = {
     enable = true;
     address = "0.0.0.0";
-
+    openFirewall = true;
   };
   environment.persistence."/permament".directories = [ "/var/lib/private/esphome" ];
 }
