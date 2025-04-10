1afd41a0d800b88ea45f6b212f29ebe6f37b2177
Probirskie <Probirskie@web.de>
2025-04-10 15:55:26 +0200

    lenovo-linux: SWAP-Datei angelegt (16GiB)

--- lenovo-linux/hardware-configuration.nix
+++ lenovo-linux/hardware-configuration.nix
@@ -46,7 +46,13 @@
     fsType = "btrfs";
   };
 
-  swapDevices = [ ];
+  # Nachträgliche SWAP-Datei wegen Abstürzen
+  swapDevices = [
+    {
+      device = "/var/swapfile";
+      size = 16384;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
