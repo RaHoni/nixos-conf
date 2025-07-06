c213938e09c53ac74eb3439070ec8382d920b311
RaHoni <honisuess@gmail.com>
2025-07-06 14:26:38 +0200

    pi-hole: Update the blocklists with https://github.com/RPiList/specials

--- server/pi-hole.nix
+++ server/pi-hole.nix
@@ -1,8 +1,189 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   ips = config.local.ips;
   ipv4 = ips."pi.hole".ipv4;
   ipv6 = ips."pi.hole".ipv6;
+  default_data = {
+    address = [ ];
+    type = "block";
+    enable = true;
+    groups = [ 0 ];
+    comment = "Added from setup";
+  };
+  add_adlists = data: ''
+    retry -t 5 curl -X POST "http://${ipv4}:80/api/lists" -H 'accept: application/json' -H 'content-type: application/json' -d '${
+      builtins.toJSON (default_data // data)
+    }'
+  '';
+  adLists = [
+    {
+      address = [
+        "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+        "https://raw.githubusercontent.com/0Zinc/easylists-for-pihole/master/easyprivacy.txt"
+        "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
+        "https://v.firebog.net/hosts/static/w3kbl.txt"
+        "https://adaway.org/hosts.txt"
+        "https://v.firebog.net/hosts/AdguardDNS.txt"
+        "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
+        "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+        "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&amp;showintro=0&amp;mimetype=plaintext"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
+        "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
+        "https://v.firebog.net/hosts/Prigent-Ads.txt"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
+        "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+        "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
+        "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
+        "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
+        "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+        "https://v.firebog.net/hosts/Prigent-Crypto.txt"
+        "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt"
+        "https://phishing.army/download/phishing_army_blocklist_extended.txt"
+        "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"
+        "https://v.firebog.net/hosts/Shalla-mal.txt"
+        "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
+        "https://urlhaus.abuse.ch/downloads/hostfile/"
+        "https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser"
+        "https://raw.githubusercontent.com/ZingyAwesome/easylists-for-pihole/master/language/german.txt"
+      ];
+      comment = "Old Added from Setup";
+    }
+    {
+      address = [
+        "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/adblock/adblock.txt"
+        "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt"
+        "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt"
+        "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/AmazonFireTV.txt"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/Win10Telemetry"
+        "https://raw.githubusercontent.com/wlqY8gkVb9w1Ck5MVD4lBre9nWJez8/W10TelemetryBlocklist/master/W10TelemetryBlocklist"
+        "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+        "https://v.firebog.net/hosts/Easyprivacy.txt"
+        "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+        "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/samsung"
+        "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt"
+        "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
+        "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/data/combined_disguised_trackers.txt"
+      ];
+      comment = "Tracking (Setup)";
+    }
+    {
+      address = [
+        #"https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/Streaming" # Zweifelhafte Streaming anbieter
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/notserious"
+        "https://raw.githubusercontent.com/Monstanner/DuckDuckGo-Fakeshops-Blocklist/main/Blockliste"
+      ];
+      comment = "Fakeshops und Abofallen (Setup)";
+    }
+    {
+      address = [
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/easylist"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/spam.mails"
+        "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
+        "https://v.firebog.net/hosts/Easyprivacy.txt"
+        "https://v.firebog.net/hosts/Easylist.txt"
+        "https://v.firebog.net/hosts/Prigent-Ads.txt"
+        "https://v.firebog.net/hosts/AdguardDNS.txt"
+        "https://adaway.org/hosts.txt"
+        "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt"
+        "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/data/combined_disguised_ads.txt"
+      ];
+      comment = "Werbung (Setup)";
+    }
+    {
+      address = [
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/malware"
+        "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/crypto"
+        "https://v.firebog.net/hosts/Prigent-Malware.txt"
+        "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+        "https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
+        "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
+        "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADomains.txt"
+        "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
+        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
+        "https://urlhaus.abuse.ch/downloads/hostfile/"
+        "https://raw.githubusercontent.com/AmnestyTech/investigations/master/2021-07-18_nso/domains.txt"
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/tif.txt"
+      ];
+      comment = "Malware (Setup)";
+    }
+    {
+      address = [
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/Phishing-Angriffe"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting3"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting4"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-0"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-1-Teil-1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-1-Teil-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-3-Teil-1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-3-Teil-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-4"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-5-Teil-1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-5-Teil-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-6"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-7-Teil-1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-7-Teil-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-8-Teil-1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-8-Teil-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-9-Teil-1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-9-Teil-2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Volks-und-Raiffeisenbank/VR-PLZ-9-Teil-3"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/BadenWuerttemberg1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/BadenWuerttemberg2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Bayern1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Bayern2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Berlin"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Brandenburg"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Bremen"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Hamburg"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Hessen1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Hessen2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/MecklenburgVorpommern"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/NRW1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/NRW2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/NRW3"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/NRW4"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Niedersachsen1"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Niedersachsen2"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/RheinlandPfalz"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Saarland"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Sachsen"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/SachsenAnhalt"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/SchleswigHolstein"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/DomainSquatting/DE/Sparkasse/Thueringen"
+        "https://raw.githubusercontent.com/namePlayer/dhl-scamlist/main/dns-blocklists/pihole-blacklist"
+        "https://www.einmalzahlungzweihundert.de/bl-einmalzahlung.txt"
+      ];
+      comment = "Phishing & Domain-Squatting (Setup)";
+    }
+    {
+      address = [
+        "https://big.oisd.nl"
+        "https://v.firebog.net/hosts/static/w3kbl.txt"
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/Fake-Science"
+        "https://raw.githubusercontent.com/SoftCreatR/fakerando-domains/main/all.txt"
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt"
+        "https://raw.githubusercontent.com/elliotwutingfeng/Inversion-DNSBL-Blocklists/main/Google_hostnames_ABP.txt"
+        "https://raw.githubusercontent.com/autinerd/anti-axelspringer-hosts/master/axelspringer-hosts"
+      ];
+      comment = "Diverses (Setup)";
+    }
+  ];
+  add_all_adlists = lib.strings.concatMapStrings add_adlists adLists;
 in
 {
   imports = [ ../generic/ips.nix ];
@@ -15,40 +196,11 @@ in
   systemd.services.pi-hole-adlists = {
     wants = [ "podman-pi-hole.service" ];
     wantedBy = [ "multi-user.target" ];
-    script = ''
-      retry -t 5 curl -X POST "http://${ipv4}:80/api/lists" -H 'accept: application/json' -H 'content-type: application/json' -d '{"address":[
-        "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
-        "https://raw.githubusercontent.com/0Zinc/easylists-for-pihole/master/easyprivacy.txt",
-        "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt",
-        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts",
-        "https://v.firebog.net/hosts/static/w3kbl.txt",
-        "https://adaway.org/hosts.txt",
-        "https://v.firebog.net/hosts/AdguardDNS.txt",
-        "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt",
-        "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt",
-        "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&amp;showintro=0&amp;mimetype=plaintext",
-        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts",
-        "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts",
-        "https://v.firebog.net/hosts/Prigent-Ads.txt",
-        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts",
-        "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt",
-        "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt",
-        "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt",
-        "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt",
-        "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt",
-        "https://v.firebog.net/hosts/Prigent-Crypto.txt",
-        "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt",
-        "https://phishing.army/download/phishing_army_blocklist_extended.txt",
-        "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt",
-        "https://v.firebog.net/hosts/Shalla-mal.txt",
-        "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt",
-        "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts",
-        "https://urlhaus.abuse.ch/downloads/hostfile/",
-        "https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser",
-        "https://raw.githubusercontent.com/ZingyAwesome/easylists-for-pihole/master/language/german.txt"
-      ],"type":"block","comment":"Added at setup","groups":[0],"enabled":true}'
-      podman exec -it pi-hole /usr/local/bin/pihole -g
-    '';
+    script =
+      add_all_adlists
+      + ''
+        podman exec -it pi-hole /usr/local/bin/pihole -g
+      '';
     path = with pkgs; [
       curl
       podman
