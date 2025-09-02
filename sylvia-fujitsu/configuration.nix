b027f62ca4de2914f5d306a829cc3113fe2376f7
RaHoni <honisuess@gmail.com>
2025-09-02 22:59:10 +0200

    sylvia-fujitsu: Finish Install

--- sylvia-fujitsu/configuration.nix
+++ sylvia-fujitsu/configuration.nix
@@ -22,7 +22,6 @@
   boot.loader.timeout = 5;
 
   networking.hostName = "sylvia-fujitsu"; # Define your hostname.
-  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
