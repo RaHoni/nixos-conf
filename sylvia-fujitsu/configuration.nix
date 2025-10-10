699cb171b90c302449e5b8d723aed040dd18208d
RaHoni <honisuess@gmail.com>
2025-10-10 21:04:13 +0200

    sylvia-fujitsu: Add mediathekview

--- sylvia-fujitsu/configuration.nix
+++ sylvia-fujitsu/configuration.nix
@@ -110,6 +110,7 @@
       avidemux # Aufruf bei Wayland mit der option --platform 'xcb'
       tvbrowser
       musescore
+      mediathekview
 
       # Tools und Hilfsprogramme
       neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
