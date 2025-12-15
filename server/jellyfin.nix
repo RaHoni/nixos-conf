faaa56a0f822ea313a21c7be29d502d5891d495c
RaHoni <honisuess@gmail.com>
2025-12-15 16:35:58 +0100

    jellyseerr: Make proxied services in torrent accessible

--- server/jellyfin.nix
+++ server/jellyfin.nix
@@ -12,6 +12,40 @@ in
     enable = true;
   };
 
+  services.nginx.virtualHosts = {
+    sonarr = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 8090;
+        }
+      ];
+      locations."/" = {
+        proxyPass = "http://unix:/var/nginx/sonarr.sock";
+      };
+    };
+    radarr = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 8091;
+        }
+      ];
+      locations."/" = {
+        proxyPass = "http://unix:/var/nginx/radarr.sock";
+      };
+    };
+    jackett = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 8092;
+        }
+      ];
+      locations."/".proxyPass = "http://unix:/var/nginx/jackett.sock";
+    };
+  };
+
   users.users.jellyfin.extraGroups = [ "render" ];
 
   environment.persistence."/permament".directories = with jellyfin; [
