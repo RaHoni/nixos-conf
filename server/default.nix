08df040e597ca2bbb379c7afdecbbab0206c7c23
RaHoni <honisuess@gmail.com>
2025-12-24 14:34:39 +0100

    server: Move networking to separate file

--- server/default.nix
+++ server/default.nix
@@ -13,6 +13,7 @@ in
     ./hardware-config.nix
     ../generic/ips.nix
     ../generic/networking.nix
+    ./networking.nix
   ];
   sops.age.keyFile = lib.mkForce "/permament/sops-nix/key.txt";
   sops.secrets = {
@@ -181,56 +182,6 @@ in
   myModules.autoUpgrade.enable = true;
   myModules.autoUpgrade.allowReboot = true;
 
-  networking = {
-    dhcpcd.IPv6rs = true;
-    useDHCP = false;
-    hostName = "server";
-    hostId = "abb92398";
-    useNetworkd = false;
-    nameservers = [
-      "127.0.0.1"
-      ips."pi.hole".ipv6.address
-      "192.168.2.1"
-      "1.1.1.1"
-    ];
-    bridges = {
-      br0 = {
-        interfaces = [ "eth0" ];
-      };
-    };
-    nat = {
-      enable = true;
-      internalInterfaces = [
-        "ve-+"
-        "veth0"
-      ];
-      externalInterface = "eth0";
-      enableIPv6 = true;
-    };
-    interfaces = {
-      br0 = {
-        macAddress = "86:2E:6C:D4:F6:1E";
-        useDHCP = true;
-        tempAddress = "disabled";
-        ipv4.addresses = [
-          {
-            address = "192.168.1.14";
-            prefixLength = 24;
-          }
-          ips.server.ipv4
-          ips.audiobookshelf.ipv4
-          ips.nebula-lighthouse.ipv4
-          ips.ssl-proxy.ipv4
-        ];
-        ipv6.addresses = [
-          ips.server.ipv6
-          ips."pi.hole".ipv6
-          ips.audiobookshelf.ipv6
-          ips.nebula-lighthouse.ipv6
-        ];
-      };
-    };
-  };
   boot.loader.systemd-boot = {
     enable = true;
     memtest86.enable = true;
---
+++ server/networking.nix
@@ -0,0 +1,56 @@
+{ config, ... }:
+let
+  ips = config.local.ips;
+in
+{
+  networking = {
+    dhcpcd.IPv6rs = true;
+    useDHCP = false;
+    hostName = "server";
+    hostId = "abb92398";
+    useNetworkd = false;
+    nameservers = [
+      "127.0.0.1"
+      ips."pi.hole".ipv6.address
+      "192.168.2.1"
+      "1.1.1.1"
+    ];
+    bridges = {
+      br0 = {
+        interfaces = [ "eth0" ];
+      };
+    };
+    nat = {
+      enable = true;
+      internalInterfaces = [
+        "ve-+"
+        "veth0"
+      ];
+      externalInterface = "eth0";
+      enableIPv6 = true;
+    };
+    interfaces = {
+      br0 = {
+        macAddress = "86:2E:6C:D4:F6:1E";
+        useDHCP = true;
+        tempAddress = "disabled";
+        ipv4.addresses = [
+          {
+            address = "192.168.1.14";
+            prefixLength = 24;
+          }
+          ips.server.ipv4
+          ips.audiobookshelf.ipv4
+          ips.nebula-lighthouse.ipv4
+          ips.ssl-proxy.ipv4
+        ];
+        ipv6.addresses = [
+          ips.server.ipv6
+          ips."pi.hole".ipv6
+          ips.audiobookshelf.ipv6
+          ips.nebula-lighthouse.ipv6
+        ];
+      };
+    };
+  };
+}
