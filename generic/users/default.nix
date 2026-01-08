8c5713f47cb59e20a9db2b5df5c0c587bffed974
RaHoni <honisuess@gmail.com>
2026-01-08 23:08:26 +0100

    users: actually export the SOPS_AGE_KEY_FILE var

--- generic/users/default.nix
+++ generic/users/default.nix
@@ -22,8 +22,10 @@ in
     autosuggestion.enable = true;
     initContent = "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh";
     localVariables = {
-      SOPS_AGE_KEY_FILE = osConfig.sops.age.keyFile;
       YSU_IGNORED_ALIASES = [ "g" ];
+    };
+    sessionVariables = {
+      SOPS_AGE_KEY_FILE = osConfig.sops.age.keyFile;
     };
   };
 
