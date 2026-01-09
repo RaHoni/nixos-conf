d28d56a20b602a73106085f85817c13bfaa64e85
RaHoni <honisuess@gmail.com>
2026-01-09 10:24:49 +0100

    raoul-framework: Fix restic auth
    It isn't possible to specify the username as part of the URI that is the
    reason the username is now part of the env secrets file

--- raoul-framework/restic.nix
+++ raoul-framework/restic.nix
@@ -5,14 +5,17 @@
       repo-passwd.sopsFile = ../secrets/raoul-framework/restic.yaml;
       restic-server-pass.sopsFile = ../secrets/raoul-framework/restic.yaml;
     };
-    templates.restic-http-pass = {
-      content = "RESTIC_REST_PASSWORD='${config.sops.placeholder.restic-server-pass}'";
+    templates.restic-http-conf = {
+      content = ''
+        RESTIC_REST_PASSWORD='${config.sops.placeholder.restic-server-pass}'
+        RESTIC_REST_USERNAME=raoul
+      '';
     };
   };
   services.restic.backups.home = {
-    repository = "rest:http://raoul@server:8050/raoul/framework";
+    repository = "rest:http://server:8080/raoul/framework";
     passwordFile = config.sops.secrets.repo-passwd.path;
-    environmentFile = config.sops.templates.restic-http-pass.path;
+    environmentFile = config.sops.templates.restic-http-conf.path;
     paths = [ "/home/raoul" ];
     exclude = [
       "/var/cache"
