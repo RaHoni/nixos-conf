e63ff388b6b4ddb62be075d82f90ce7230cd487e
RaHoni <honisuess@gmail.com>
2026-01-14 13:06:18 +0100

    ssh: move ssh config to separate file

--- generic/users/default.nix
+++ generic/users/default.nix
@@ -5,12 +5,11 @@
   osConfig,
   ...
 }:
-with lib;
-let
-  sshIdentity = keyname: "~/.ssh/keys/${keyname}.pub";
-in
 {
-  imports = [ ./stylix_fix.nix ];
+  imports = [
+    ./stylix_fix.nix
+    ./ssh.nix
+  ];
   home.file = {
     ".ssh/keys".source = ../sshPubkeys;
     #".zshrc".source = ./zshrc;
@@ -35,97 +34,6 @@ in
       enable = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
-    };
-
-    ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      matchBlocks = rec {
-        cat-laptop = {
-          user = "cathach";
-          identityFile = sshIdentity "support";
-        };
-        stereoanlage = {
-          user = "root";
-          hostname = "stereoanlage.localdomain";
-          identityFile = sshIdentity "id_ed25519_kodi";
-        };
-        vps = {
-          user = "root";
-          hostname = "212.227.135.200";
-          identityFile = sshIdentity "id_strato";
-        };
-        rescueIso = {
-          user = "nixos";
-          identityFile = sshIdentity "support";
-          forwardAgent = true;
-        };
-        streaming = {
-          hostname = "petronillastreaming";
-          user = "streaming";
-          identityFile = sshIdentity "support";
-          forwardAgent = true;
-        };
-        jasmine = {
-          hostname = "jasmine-laptop";
-          user = "jasmine";
-          identityFile = sshIdentity "support";
-          forwardAgent = true;
-        };
-        "github.com" = {
-          #hostname = "ssh.github.com";
-          user = "git";
-          port = 22;
-          identityFile = sshIdentity "github";
-        };
-        r-desktop = {
-          hostname = "r-desktop";
-          identityFile = sshIdentity "r-desktop-ed25519";
-          forwardAgent = true;
-        };
-        surface = {
-          hostname = "surface-raoul-nixos";
-          identityFile = sshIdentity "Surface_id_ed25519";
-          forwardAgent = true;
-        };
-        honermannmedia = {
-          hostname = "honermannmedia";
-          user = "root";
-          identityFile = sshIdentity "id_ed25519_kodi";
-        };
-
-        lenovo-linux = {
-          hostname = "lenovo-linux";
-          identityFile = sshIdentity "id_rsa_lenovo-linux";
-          forwardAgent = true;
-        };
-        sylvia-fujitsu = {
-          hostname = "sylvia-fujitsu";
-          user = "sylvia";
-          identityFile = [
-            (sshIdentity "id_rsa_sylvia")
-            (sshIdentity "ch-fujitsu")
-          ];
-          forwardAgent = true;
-        };
-        server = {
-          #match = ''exec "grepcidr '192.168.3.1/24 fd00::4:1/112' <(host %h) <(echo %h) &>/dev/null"'';
-          hostname = "server";
-          identityFile = [
-            (sshIdentity "id_ecdsa_proxmox")
-            (sshIdentity "ch-kodi")
-          ];
-          user = "root";
-          forwardAgent = true;
-        };
-        server-extern = server // {
-          hostname = "honermann.info";
-        };
-        "*" = {
-          forwardAgent = false;
-          identitiesOnly = true;
-        };
-      };
     };
 
     git = {
