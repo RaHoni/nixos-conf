26317dcae12d4fd320a72d9345ac3dfd3ac25f21
RaHoni <honisuess@gmail.com>
2026-06-04 20:09:12 +0200

    26.05/kanidm: enableServer was renamed to server.enable

--- server/kanidm.nix
+++ server/kanidm.nix
@@ -79,7 +79,7 @@ in
 
   services.kanidm = {
     package = pkgs.kanidmWithSecretProvisioning_1_10;
-    enableServer = true;
+    server.enable = true;
     serverSettings = {
       bindaddress = "0.0.0.0:443";
       ldapbindaddress = "0.0.0.0:3636";
