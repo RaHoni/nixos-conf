b5b1a3f22b1e7e84ed2bf6f28567ead4c9d73c4c
RaHoni <honisuess@gmail.com>
2026-06-23 08:52:06 +0200

    flake: Update lanzaboote-stable

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
 
     lanzaboote-stable = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
